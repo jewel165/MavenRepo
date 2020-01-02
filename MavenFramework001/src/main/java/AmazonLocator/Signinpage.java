52f475e32d524ac64db0462e162e1c11d8e80c3e
jewel165 <alammd1989@gmail.com>
2020-01-02 13:53:20 -0500

    RestartFramework

--- MavenFramework001/src/main/java/AmazonLocator/Signinpage.java
+++
@@ -1,9 +0,0 @@
-package AmazonLocator;
-
-public class Signinpage {
-	
-	public String Accountlist = "//*[@id=\"nav-link-accountList\"]";
-	public String emailfield = "//*[@id=\"ap_email\"]";
-	public String ContinueBTN = "//*[@id=\"continue\"]";
-
-}
--- MavenFramework001/src/main/java/AmazonValues/amazonSignIN.java
+++
@@ -1,8 +0,0 @@
-package AmazonValues;
-
-public class amazonSignIN {
-	
-	public String emailADD = "mdalam1989@outlool.com";
-	 
-
-}
